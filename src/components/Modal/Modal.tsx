da09dcf52ac6ecd0c54bda1475cbc650787b9d68
Felipe Pessina <fs.pessina@gmail.com>
2023-07-13 13:22:07 +0600

    Modal with keyboard working

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Modal as NativeModal, ModalProps as NativeModalProps, View } from 'react-native';
+import {
+  Modal as NativeModal,
+  ModalProps as NativeModalProps,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
 import { SafeArea } from '../Containers/SafeArea';
 import styled from 'styled-components/native';
 import { Text } from '../Text/Text';
 import { Icon } from '../Icon/Icon';
+import { styles } from '../../styles/styles';
 
 interface Props extends NativeModalProps {
   title?: string;
@@ -16,38 +23,45 @@ export const Modal: React.FC<Props> = ({ children, title, ...props }) => {
       transparent={false}
       {...props}
     >
-      <Overlay>
-        <Content>
-          <Header>
-            <Text size="2xl">{title}</Text>
-            <Icon
-              name="close-circle-fill"
-              onPress={props.onRequestClose}
-            />
-          </Header>
-          {children}
-        </Content>
-      </Overlay>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.full}
+      >
+        <Overlay>
+          <ScrollView contentContainerStyle={styles.fullCenter}>
+            <Content>
+              <Header>
+                <Text size="2xl">{title}</Text>
+                <Icon
+                  name="close-circle-fill"
+                  onPress={props.onRequestClose}
+                />
+              </Header>
+              {children}
+            </Content>
+          </ScrollView>
+        </Overlay>
+      </KeyboardAvoidingView>
     </NativeModal>
   );
 };
 
 const Overlay = styled(SafeArea)`
   background-color: ${({ theme }) => theme.colors.overlay};
-  justify-content: center;
-  align-items: center;
+  flex: 1;
 `;
 
-const Content = styled(View)`
+const Content = styled.View`
   background-color: ${({ theme }) => theme.colors.background.DEFAULT};
   padding: ${({ theme }) => theme.space[4]}px;
   border-radius: ${({ theme }) => theme.borderRadius.lg};
   max-width: 80%;
   max-height: 70%;
   min-width: 200px;
+  align-self: center;
 `;
 
-const Header = styled(View)`
+const Header = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -4,4 +4,9 @@ export const styles = StyleSheet.create({
   full: {
     flexGrow: 1,
   },
+  fullCenter: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
